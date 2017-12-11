39710d8b0ff264f98346db26edff212d19a1a252
Annette Coll <annette.coll@outlook.com>
2017-12-11 14:45:34 -0500

    pseudocode draft1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+//console.log('Hello World')
+
+// Game objective: Player enter letters to guess the word. The word is hidden and represented by 
+// blank spaces that will need to be filled in to win. Everytime the player guesses wrong.
+
+
+// Pseudocode:
+// Nine lives (A Hangman inspired game): 
+// Random word of ten-twelve letters long is selected. 
+
+// Show player hidden word they have to guess. Show progress.
+// Get them to guess.
+
+// Tell player to pick a single letter.
+
+// If guess is in the word. Update with progress.
+// Else alert they've lost their life!
+
