8fb27bdfe0a86ef5d1594939e8954e546330e5e4
Annette Coll <annette.coll@outlook.com>
2017-12-11 15:02:48 -0500

    hangman words added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Pseudocode:
 // Nine lives (A Hangman inspired game): 
-// Random word of ten-twelve letters long is selected. 
+// Random word of ten-twelve letters long are selected by. 
 
 // Show player hidden word they have to guess. Show progress.
 // Get them to guess.
@@ -14,5 +14,29 @@
 // Tell player to pick a single letter.
 
 // If guess is in the word. Update with progress.
-// Else alert they've lost their life!
+//      Else alert they only have 8 more guesses!
+// If word is guessed right. 
+//      Congratulate player!
+// Else mark down each letter.
+// If all nine tries are used. 
+//      Tell player they lost.
+// Loop back to restart the game.
 
+var words = [
+    'strawberry'
+    'blackjacks'
+    'lumberjack'
+    'television'
+    'Retirement'	
+    'jackhammer'
+    'mozzarella'
+    'equalizing'
+    'jeopardize'
+    'quizmaster'
+    'quicksteps'
+    'beachcombs'
+    'schematize'
+    'bankruptcy'
+    'inadequacy'
+    'civilizers'
+]
